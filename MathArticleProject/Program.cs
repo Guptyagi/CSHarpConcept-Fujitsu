6c7ae683c31fce367ed8e4329660b1522306b969
Guptyagi <kyc.ricex.user1@gmail.com>
2022-05-26 17:32:24 +0530

    Selenium Concepts added

--- MathArticleProject/Program.cs
+++ MathArticleProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +16,11 @@ namespace MathArticleProject
         {
 
             ChromeDriver driver = new ChromeDriver();
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
-            String  curentURL=driver.Url;
+            //driver.Url = "https://opensource-demo.orangehrmlive.com/";
+            driver.Url = "https://www.facebook.com";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(30);
+            /*String  curentURL=driver.Url;
             Console.WriteLine(curentURL);
 
            String title= driver.Title;
@@ -31,7 +35,31 @@ namespace MathArticleProject
             driver.FindElement(By.Id("txtPassword")).SendKeys("admin123");
             driver.FindElement(By.Id("btnLogin")).Click();
             driver.FindElement(By.Id("welcome")).Click();
-            driver.FindElement(By.LinkText("Logout"));
+            driver.FindElement(By.LinkText("Logout"));*/
+            driver.Url = "https://www.facebook.com";
+            driver.FindElement(By.LinkText("Create New Account")).Click();
+            driver.FindElement(By.Name("firstname")).SendKeys("John");
+            driver.FindElement(By.Name("lastname")).SendKeys("Wick");
+            driver.FindElement(By.Id("password_step_input")).SendKeys("welcome@123");
+
+            //Enter the Date
+            SelectElement selectDate = new SelectElement(driver.FindElement(By.Id("day")));
+            selectDate.SelectByText("15");
+            //Enter Month
+            SelectElement selectMonth = new SelectElement(driver.FindElement(By.Id("month")));
+            selectMonth.SelectByText("Dec");
+            //Enter Year
+
+            SelectElement selectYear = new SelectElement(driver.FindElement(By.Id("year")));
+            selectYear.SelectByText("2000");
+
+
+            driver.FindElement(By.XPath("//input[@value='-1']")).Click();
+            driver.FindElement(By.Name("websubmit")).Click();
+
+
+
+
 
         }
 
